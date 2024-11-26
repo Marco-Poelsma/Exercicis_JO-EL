225b31f86bfa8ee9a1d0492ab2ca93e814ecacc8
Ioan Pavel Lupan Trifan <pavellupan229@gmail.com>
2024-11-26 10:33:49 +0100

    abba arreglado

--- Main.java
+++ Main.java
@@ -32,36 +32,5 @@ public class Abba {
             if (c <= '\u007F') sb.append(c);
         }
         return sb.toString();
-
-public class Vay Baya{
-    public static void main (String[] args) {
-        Scanner scan = new Scanner (System.in);
-        int casos = scan.nextInt();
-        scan.nextLine();
-        for (int i = 0; i < casos; i++) {
-            //Canviar "b" per "v" i "v" per "b"
-            String frase = scan.nextLine();
-            for (char c : frase.toCharArray()) {
-                switch (c) {
-                    case 'b':
-                        System.out.print('v');
-                        break;
-                    case 'v':
-                        System.out.print('b');
-                        break;
-                    case 'B':
-                        System.out.print('V');
-                        break;
-                    case 'V':
-                        System.out.print('B');
-                        break;
-                    default:
-                        System.out.print(c);
-                        break;
-                }
-            }
-            System.out.println();
-        }
-        scan.close();
     }
 }
