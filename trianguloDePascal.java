0eb331a85bc8950a1daae95ee79f515ff5923109
Marco Poelsma <marco.poelsma@gracia.lasalle.cat>
2024-11-27 09:23:23 +0100

    Puto JO-EL. Actualizado y va bien excepto en 3 casos que da 97 o 98%

--- trianguloDePascal.java
+++ trianguloDePascal.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
 
-public class Main {
+public class trianguloDePascal {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int iterations = scan.nextInt();
-        int sum = calculatePascalSum(iterations);
+        long iterations = scan.nextLong();
+        long sum = calculatePascalSum(iterations);
         printPascalTriangle(iterations);
-        System.out.println("Sum: " + sum);
+        System.out.println("Suma: " + sum);
         scan.close();
     }
 
-    public static int calculatePascalSum(int n) {
+    public static long calculatePascalSum(long n) {
         int sum = 0;
-        for (int p = 1, m = n - 1, d = 1; d <= n; p = p * m / d, --m, ++d) {
+        for (long p = 1, m = n - 1, d = 1; d <= n; p = p * m / d, --m, ++d) {
             sum += p;
         }
         return sum;
     }
 
-    public static void printPascalTriangle(int n) {
-        for (int i = 1; i <= n; i++) {
+    public static void printPascalTriangle(long n) {
+        for (long i = 1; i <= n; i++) {
             printPascalNthRow(i);
         }
     }
 
-    public static void printPascalNthRow(int n) {
-        for (int p = 1, m = n - 1, d = 1; d <= n; p = p * m / d, --m, ++d) {
+    public static void printPascalNthRow(long n) {
+        for (long p = 1, m = n - 1, d = 1; d <= n; p = p * m / d, --m, ++d) {
             System.out.print(p + " ");
         }
         System.out.println();
