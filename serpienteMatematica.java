233bd984d8043cef1dea511d3c9aa4b6ed086020
Marco Poelsma <marco.poelsma@gracia.lasalle.cat>
2024-11-27 09:46:20 +0100

    Ejercicio Serpiente Matemática (Liga) + Arreglar Diada Castellera (Liga)

--- serpienteMatematica.java
+++ serpienteMatematica.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class serpienteMatematica {
     public static void main(String[] args) {
         Scanner scan = new Scanner (System.in);
         int n = scan.nextInt();
