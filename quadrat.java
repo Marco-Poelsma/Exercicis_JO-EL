2615b9ea6e8cba9476ce89806f33734775116df1
Marco Poelsma <marco.poelsma@gracia.lasalle.cat>
2024-11-29 13:12:44 +0100

    Quadrat

---
+++ quadrat.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class quadrat {
+    public static void main(String[] args) {
+        //Declaració de variables
+        Scanner scan = new Scanner (System.in);
+        int longitud = scan.nextInt();
+        System.out.println((longitud*longitud));
+    }
+}
