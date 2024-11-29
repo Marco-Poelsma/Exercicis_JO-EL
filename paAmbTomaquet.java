c9dd9ed6ed6a33cb7b100e42f000a383cb2293aa
Marco Poelsma <marco.poelsma@gracia.lasalle.cat>
2024-11-29 13:14:46 +0100

    Pa amb Tomàquet

---
+++ paAmbTomaquet.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class paAmbTomaquet{
+    public static void main (String[] args){
+        Scanner scan = new Scanner (System.in);
+        String primeraParaula = scan.nextLine();
+        String segonaParaula = scan.nextLine();
+        System.out.println (primeraParaula + " amb " + segonaParaula);
+    }
+}
