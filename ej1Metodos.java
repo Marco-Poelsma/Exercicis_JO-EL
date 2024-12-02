3b4b33f2d98ab6382cdf68aea1b232803c604c6e
Marco Poelsma <marco.poelsma@gracia.lasalle.cat>
2024-12-02 09:03:26 +0100

    Ej1 actualitzat

--- ej1Metodos.java
+++ ej1Metodos.java
@@ -6,10 +6,16 @@ public class ej1Metodos {
             System.out.println(i);
         }
     }
+    public static void benvinguda (String input) {
+        System.out.println("Hola, " + input + "!");
+    }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         compteEnrere(n);
+        scan.nextLine();
+        String input = scan.nextLine();
+        benvinguda(input);
         scan.close();
     }
 }
