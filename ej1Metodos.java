d97c05aaf8b09d8e984a2128043f36f3ef0748b9
Marco Poelsma <marco.poelsma@gracia.lasalle.cat>
2024-12-02 08:46:07 +0100

    Mètodes. Exercici 1.

---
+++ ej1Metodos.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ej1Metodos {
+    public static void compteEnrere(int n) {
+        for (int i = n; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        compteEnrere(n);
+    }
+}
